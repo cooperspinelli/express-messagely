85c5b54531422cbdfff3c1c7728d8585349ff50a
David Sapiro <dsap48@gmail.com>
2024-03-25 11:37:52 -0700

    added authenticate updateLoginTimestamp and all user methods

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@
 
 const bcrypt = require("bcrypt");
 const { BCRYPT_WORK_FACTOR } = require("../config");
+const { NotFoundError } = require("../expressError");
 
 /** User of the site. */
 
@@ -13,7 +14,7 @@ class User {
 
   static async register({ username, password, first_name, last_name, phone }) {
 
-    const hashed_password = bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const hashed_password = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
 
     const result = await db.query(
       `INSERT INTO users (username,
@@ -35,17 +36,42 @@ class User {
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+      FROM users
+      WHERE username = $1`, [username]
+    );
+
+    if (!result.rows[0]) throw new NotFoundError("User not found.");
+
+    const hashed_password = result.rows[0].password;
+
+    return await bcrypt.compare(hashed_password, password);
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+      SET last_login_at = current_timestamp
+      WHERE username = $1,
+      RETURNING username`, [username]
+    );
+
+    if (!result.rows[0]) throw new NotFoundError("User not found.");
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const results = await db.query(
+      `SELECT username, first_name, last_name
+      FROM users`
+    );
+
+    return results.rows;
   }
 
   /** Get: get user by username
